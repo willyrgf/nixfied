7baee3afc1e62b13465a5cd91fb8cf9c9b07e4b0
Willy Romão <willyr.goncalves@gmail.com>
2026-04-03 11:38:51 +0800

    extract runtime bootstrap

--- nixfied/framework/runtime/executor.nix
+++ nixfied/framework/runtime/executor.nix
@@ -51,6 +51,14 @@ let
     else
       "nixfied-model.json"
   ) (builtins.toJSON model);
+  controlBootstrapShell = import ./control-bootstrap.nix {
+    inherit
+      pkgs
+      model
+      projectRoot
+      modelFile
+      ;
+  };
   shellCommon = import ../core/shell-common.nix { inherit pkgs; };
   skipPolicy = import ../core/skip-policy.nix { inherit pkgs; };
   registryShell = registry.events.mkShellLib { };
@@ -97,44 +105,13 @@ pkgs.writeShellScriptBin "nixfied-executor" ''
         export NIXFIED_EXECUTOR_BIN="$0"
         export NIXFIED_EXECUTOR_SELF="$0"
 
-        MODEL_FILE=${pkgs.lib.escapeShellArg (builtins.toString modelFile)}
-        export NIXFIED_MODEL_FILE="$MODEL_FILE"
-        PROJECT_ROOT=${pkgs.lib.escapeShellArg (builtins.toString projectRoot)}
-        REGISTRY_ROOT_DEFAULT="${model.state.policy.registryRoot}"
-        ARTIFACTS_ROOT_DEFAULT="${model.state.policy.artifactsRoot}"
-        if [ -n "''${NIXFIED_RUNTIME_REGISTRY_ROOT+x}" ]; then
-          REGISTRY_ROOT_DEFAULT="$NIXFIED_RUNTIME_REGISTRY_ROOT"
-        elif [ -n "''${NIXFIED_RUNTIME_DIR_BASE+x}" ]; then
-          REGISTRY_ROOT_DEFAULT="$NIXFIED_RUNTIME_DIR_BASE/registry"
-        fi
-        if [ -n "''${NIXFIED_RUNTIME_ARTIFACTS_DIR+x}" ]; then
-          ARTIFACTS_ROOT_DEFAULT="$NIXFIED_RUNTIME_ARTIFACTS_DIR"
-        elif [ -n "''${NIXFIED_RUNTIME_DIR_BASE+x}" ]; then
-          ARTIFACTS_ROOT_DEFAULT="$NIXFIED_RUNTIME_DIR_BASE/artifacts"
-        fi
-        if [ -n "''${REGISTRY_ROOT+x}" ]; then
-          REGISTRY_ROOT_EXPLICIT=1
-        else
-          REGISTRY_ROOT_EXPLICIT=0
-        fi
-        REGISTRY_ROOT="''${REGISTRY_ROOT:-$REGISTRY_ROOT_DEFAULT}"
-        RUN_ID_ACTIVE_ROOT="$REGISTRY_ROOT/active"
-        RUN_ID_COUNTER_ROOT="$REGISTRY_ROOT/counters"
-
+        ${controlBootstrapShell}
         ${registryShell}
         ${envSandboxShell}
         ${runtimeHandoffShell}
         ${artifactsRuntimeShell}
         ${sharedRuntimeLibShell}
         ${skipPolicy.skipPolicyFunctions}
-
-        sha256_text() {
-          printf '%s' "$1" | ${pkgs.coreutils}/bin/sha256sum | ${pkgs.gawk}/bin/awk '{print $1}'
-        }
-
-        kernel_event_detail() {
-          ${kernelPackage}/bin/nixfied-kernel event-detail render "$@"
-        }
 
         workflow_phase_service_set_program() {
           local service_set_id="$1"
@@ -191,16 +168,6 @@ pkgs.writeShellScriptBin "nixfied-executor" ''
         LAST_WORKFLOW_SUMMARY_FAILED_COUNT="0"
         LAST_WORKFLOW_SUMMARY_SKIPPED_COUNT="0"
         LAST_WORKFLOW_SUMMARY_CANCELED_COUNT="0"
-
-        compute_attempt_id() {
-          local attempt_dir
-          local attempt_name
-
-          attempt_dir="$(mktemp -d "''${TMPDIR:-/tmp}/nixfied-attempt.XXXXXX")" || return 1
-          attempt_name="$(basename "$attempt_dir")"
-          rmdir "$attempt_dir"
-          printf '%s' "attempt-''${attempt_name#nixfied-attempt.}"
-        }
 
         append_event() {
           local run_id="$1"
