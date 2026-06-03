ea37ba3f6dc69b406d10ee222fd68af6b8511cf9
Willy Romão <willyr.goncalves@gmail.com>
2026-06-03 17:49:24 -0300

    model: generate minimal views

--- nix/compiler/emit-model.nix
+++ nix/compiler/emit-model.nix
@@ -2,14 +2,31 @@
 
 let
   modelJson = builtins.toJSON derived.model;
+  views = import ./views.nix { model = derived.model; };
+  schemaJson = builtins.toJSON views.schema;
+  capabilitiesJson = builtins.toJSON views.capabilities;
 in
 pkgs.runCommand "nixfied-model"
   {
     m0Helper = derived.package;
-    passAsFile = [ "modelJson" ];
-    inherit modelJson;
+    passAsFile = [
+      "modelJson"
+      "schemaJson"
+      "capabilitiesJson"
+      "docsMarkdown"
+    ];
+    inherit
+      modelJson
+      schemaJson
+      capabilitiesJson
+      ;
+    docsMarkdown = views.docs;
   }
   ''
     mkdir -p "$out"
+    mkdir -p "$out/views"
     cp "$modelJsonPath" "$out/model.json"
+    cp "$schemaJsonPath" "$out/views/schema.json"
+    cp "$capabilitiesJsonPath" "$out/views/capabilities.json"
+    cp "$docsMarkdownPath" "$out/views/docs.md"
   ''
